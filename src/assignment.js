60c6b1fa99e91152529a973a90ce909f09bc4b05
Adeyemi Blessing <adeyemiblessing124@gmail.com>
2023-08-30 20:08:30 +0000

    adeyemi blessing attempted question 1 & 3

--- src/assignment.js
+++ src/assignment.js
@@ -16,11 +16,14 @@ const assignment = {};
  */
 function sumOfNumbersTo(destination) {
     let sum = 0;
+    for (let i= 1; i<= destination; i++){
+        sum += i;
+    }
     return sum
 }
 
 
-// assignment.sumOfNumbersTo = sumOfNumbersTo;
+ assignment.sumOfNumbersTo = sumOfNumbersTo;
 
 /**
  * Challenge - 2
@@ -68,11 +71,16 @@ function countEvenNumbersWithin(destination) {
  */
 function celsiusToFahrenheit(arrayOfNumbers) {
     let result = [];
+    for (let celcius of arrayOfNumbers) {
+        // Conversion formula: Fahrenheit = Celcius *9/5 + 32
+        let farenheit = Math.trunc(celcius*9/5+32);
+        result.push(farenheit);
+    }
 
     return result;
 }
 
-// assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+ assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 // ========================
 // DO NOT EDIT THIS BLOCK
